967bef6fb1c521d45c7e4c0ed97d448c57797968
RiceDaddy321 <0carlitos2@gmail.com>
2022-01-24 15:22:23 -0600

    Initial Commit

---
+++ trendtrakcer.cpp
@@ -0,0 +1,31 @@
+#include "trendtracker.h"
+
+Trendtracker::Trendtracker(string filename)
+{
+
+}
+
+int Trendtracker::size()
+{
+
+}
+
+void Trendtracker::tweeted(string ht)
+{
+
+}
+
+int Trendtracker::popularity(string name)
+{
+
+}
+
+string Trendtracker::top_trend()
+{
+
+}
+
+void Trendtracker::top_three_trends(vector<string> &T)
+{
+    
+}
