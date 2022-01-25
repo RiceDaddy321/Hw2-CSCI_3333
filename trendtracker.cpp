22739375dfe880c8089aa704639ce7b46adb2f14
RiceDaddy321 <0carlitos2@gmail.com>
2022-01-24 20:48:35 -0600

    Completed clean slate

--- trendtracker.cpp
+++ trendtracker.cpp
@@ -7,7 +7,7 @@ Trendtracker::Trendtracker(string filename)
 
 int Trendtracker::size()
 {
-
+    return 0;
 }
 
 void Trendtracker::tweeted(string ht)
@@ -17,12 +17,12 @@ void Trendtracker::tweeted(string ht)
 
 int Trendtracker::popularity(string name)
 {
-
+    return 0;
 }
 
 string Trendtracker::top_trend()
 {
-
+    return "";
 }
 
 void Trendtracker::top_three_trends(vector<string> &T)
